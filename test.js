867bba15003c1f5eb3dc44632824ed5b1bd482d5
Kevin Lanni <therealklanni@gmail.com>
2017-04-21 16:12:27 -0700

    Add abort test

--- test.js
+++ test.js
@@ -1,13 +1,28 @@
 import test from 'tape'
 import Yl from './lib'
 
-test('yl', t => {
-  t.plan(1000)
+test('complete iteration', t => {
+  const n = 1000
+
+  t.plan(n + 1)
 
   let ret = Yl(i => {
-    t.ok(i+1, 'iteration '+ i)
+    t.ok(i + 1, 'iteration ' + (n - i))
     return i ? !i : 'done'
-  }, 999)
+  }, n)
 
   t.equal(ret, 'done', 'return value is ok')
+})
+
+test('aborted early iteration', t => {
+  const n = 1000
+
+  t.plan(51)
+
+  let ret = Yl(i => {
+    t.ok(i + 1, 'iteration ' + (n + i))
+    return i % 50 ? !i : i
+  }, n)
+
+  t.equal(ret, 950, 'return value is ok')
 })
